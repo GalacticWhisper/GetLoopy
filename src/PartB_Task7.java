b744db7649c5c2a51862aab52ec7ca1bdbfb149e
IanM <ianmarr@Ians-MacBook-Air.local>
2024-10-07 11:28:24 -0400

    InitialCommit

---
+++ src/PartB_Task7.java
@@ -0,0 +1,14 @@
+public class PartB_Task7 {
+    public static void main(String[] args) {
+
+        for(int row = 1; row <= 5; row++)
+        {
+            System.out.print(" * ");
+            for(int col = 1; col <= 4; col++)
+            {
+                System.out.printf(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
